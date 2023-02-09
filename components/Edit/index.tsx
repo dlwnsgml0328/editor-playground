026ed5ec4e7730b8e18a2b30632c8d39f9f04d3a
dlwnsgml0328 <junhee@eazel.net>
2023-02-09 18:34:11 +0900

    [feat] connect with iframe

--- components/Edit/index.tsx
+++ components/Edit/index.tsx
@@ -1,9 +1,113 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+interface previewData {
+  html: string;
+  mainCss: string;
+  sectionCss: string;
+}
+
+const scripts = [
+  'assets/scripts/glide/glide.min.js',
+  'assets/scripts/navbar/navbar.min.js',
+  'box/box-flex.js',
+  'https://cdnjs.cloudflare.com/ajax/libs/smoothscroll/1.4.10/SmoothScroll.min.js',
+];
 
 const EditComponent = () => {
+  // for make a POST request to server
+  const [data, setData] = useState<previewData>({ html: '', mainCss: '', sectionCss: '' });
+
+  const receiveMessage = (event: MessageEvent<any>) => {
+    if (!event.data.hasOwnProperty('html')) return;
+
+    document.head.insertAdjacentHTML('beforeend', event.data.mainCss);
+    document.head.insertAdjacentHTML('beforeend', event.data.sectionCss);
+
+    scriptInjection();
+    domInjection(event.data);
+
+    setData({
+      html: event.data.html,
+      mainCss: event.data.mainCss,
+      sectionCss: event.data.sectionCss,
+    });
+  };
+
+  const domInjection = (data: previewData) => {
+    if (typeof data.html !== 'string')
+      return alert('Can not make a preview! Save the contents first');
+
+    const wrapper = document.querySelector('.is-wrapper');
+    if (!wrapper) return alert('There is no wrapper element');
+    wrapper.innerHTML = '';
+    const range = document.createRange();
+    range.setStart(wrapper, 0);
+    wrapper.appendChild(range.createContextualFragment(data.html));
+  };
+
+  const scriptInjection = () => {
+    for (let eachScript of scripts) {
+      const script = document.createElement('script');
+      script.src = eachScript;
+      script.type = 'text/javascript';
+      document.body.appendChild(script);
+    }
+  };
+
+  const doSomething = () => {
+    // path or information about each article
+    const MAGAZINE = 'junhee/1';
+
+    const iframe = document.querySelector('.wrapper-iframe') as HTMLIFrameElement;
+    if (!iframe) return alert('Can not find iframe');
+    if (!iframe.contentWindow) return alert('Can not find iframe.contentWindow');
+
+    console.log('message to iframe!');
+
+    iframe.contentWindow.postMessage(MAGAZINE, '*');
+  };
+
+  useEffect(() => {
+    window.addEventListener('message', receiveMessage, false);
+
+    return () => {
+      window.removeEventListener('message', receiveMessage, false);
+    };
+  }, []);
+
   return (
     <div>
-      <h1>Hello Editor!</h1>
+      <button type='button' onClick={() => (window.location.href = '/')}>
+        home
+      </button>
+      <button type='button' onClick={doSomething}>
+        message
+      </button>
+
+      <h1>Editor</h1>
+      <iframe
+        className='wrapper-iframe'
+        src='http://localhost:3001/edit'
+        style={{
+          margin: 0,
+          border: '2px solid',
+          width: '100%',
+          height: '80vh',
+          boxSizing: 'border-box',
+        }}
+      />
+
+      <h1>Rendering View</h1>
+      <div
+        style={{
+          margin: 0,
+          border: '2px solid',
+          width: '100%',
+          height: '80vh',
+          boxSizing: 'border-box',
+        }}
+      >
+        <div className='is-wrapper'></div>
+      </div>
     </div>
   );
 };
--- pages/edit/index.tsx
+++ pages/edit/index.tsx
@@ -1,9 +1,22 @@
+/* eslint-disable @next/next/no-css-tags */
 import React from 'react';
 import dynamic from 'next/dynamic';
+import Head from 'next/head';
+
 const EditComponent = dynamic(() => import('../../components/Edit'), { ssr: false });
 
 const Edit = () => {
-  return <EditComponent />;
+  return (
+    <>
+      <Head>
+        <link rel='stylesheet' href='assets/minimalist-blocks/content.css' />
+        <link rel='stylesheet' href='box/box-flex.css' />
+        <link rel='stylesheet' href='assets/scripts/navbar/navbar.css' />
+      </Head>
+
+      <EditComponent />
+    </>
+  );
 };
 
 export default Edit;
